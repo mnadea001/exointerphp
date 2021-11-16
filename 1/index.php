4611783adca54b9bbd9018bde9ef514c7aaae4b3
Axel Gromat <axel.gromat@gmail.com>
2021-11-16 09:38:10 +0100

    Fixed exo 1

--- 1/index.php
+++ 1/index.php
@@ -11,7 +11,7 @@
 <body>
     <h1>Exercice 1 : Identité</h1>
     <p>A l'aide d'un formulaire HTML et de PHP, affichez les données qu'un utilisateur aura rempli dans un formulaire
-        via une requête POST</p>
+        via une requête POST SUR LA MEME PAGE</p>
     <small>Ne pas oublier de préparer le cas où les données sont non renseignées et/ou n'ont pas encore pu être
         remplies</small>
     <ul>
