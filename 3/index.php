dc0c68754dc57b4c4b736ebf74d3eb796f92fd32
Axel Gromat <axel.gromat@gmail.com>
2021-11-16 09:56:27 +0100

    added exercices 2 to 5

---
+++ 3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Exercice 3 : Inscription</h1>
+    <p>A l'aide d'un formulaire HTML et de PHP, simulez un formulaire d'inscription (avec hashage du mot de passe)
+        via une requête POST sur une page d'affichage et une page de processing. Si la connexion est effective alors on
+        affiche un message de bienvenue à l'utilisateur contenant son username.</p>
+    <small>Ne pas oublier de préparer le cas où les données sont non renseignées et/ou n'ont pas encore pu être
+        remplies. Ne pas oublier d'initialiser la session. (Ici on part du principe qu'on est connectés dès que l'on
+        s'inscrit.)</small>
+    <ul>
+        <li>username</li>
+        <li>password</li>
+        <li>confirmation password</li>
+        <li>email</li>
+    </ul>
+</body>
+
+</html>
